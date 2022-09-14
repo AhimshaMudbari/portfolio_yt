a947a372d01c540c1f65e1677cdce5e0041b1247
Ahim11 <ahim.mudbari@gmail.com>
2022-09-14 22:00:07 +0545

    navbar component

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Navbar.css';
+
+export const Navbar = () => {
+  return (
+    <div>
+      <div className="n-wrapper">
+        <div className="n-left">
+          <div className="n-name">Ahimsha</div>
+          <span>toggle btn</span>
+        </div>
+        <div className="n-right">
+          <div className="n-list">
+            <ul style={{ listStyleType: 'none' }}>
+              <li>Home</li>
+              <li>Services</li>
+              <li>Experience</li>
+              <li>Portfolio</li>
+              <li>Testimonial</li>
+            </ul>
+          </div>
+          <button className="button n-button">Contact me</button>
+        </div>
+      </div>
+    </div>
+  );
+};
