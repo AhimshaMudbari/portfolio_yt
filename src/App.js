360344ebf4146bb8563c973fff9126121c1356c4
Ahim11 <ahim.mudbari@gmail.com>
2022-09-17 21:47:17 +0545

    Components holder root

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
+import Experience from './components/Experience/Experience';
 import Intro from './components/Intro/Intro';
 import { Navbar } from './components/Navbar/Navbar';
+import Services from './components/Services/Services';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Intro />
+      <Services />
+      <Experience />
     </div>
   );
 }
