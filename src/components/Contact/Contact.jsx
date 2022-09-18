c1848a13b1732f4f21f9e0a8171b69a41f3034e6
Ahim11 <ahim.mudbari@gmail.com>
2022-09-18 22:53:00 +0545

    Contact component of a build

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import './Contact.css';
+import emailjs from '@emailjs/browser';
+import { useRef } from 'react';
+
+const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        'service_64gfu7o',
+        'template_q3trkvb',
+        form.current,
+        'WxQ7wYXZCDiyflHHs'
+      )
+      .then(
+        (result) => {
+          alert('Send successfully');
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+  return (
+    <div className="contact-form">
+      <div className="w-left">
+        <div className="awesome">
+          <span>Contact me</span>
+          <div className="s-blur1"></div>
+        </div>
+      </div>
+      <div className="c-right">
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            name="user_name"
+            className="user"
+            placeholder="Full Name..."
+            required
+          />
+          <input
+            type="email"
+            name="user_email"
+            className="user"
+            placeholder="Email"
+            required
+          />
+          <textarea
+            name="message"
+            className="user"
+            placeholder="message"
+            required
+          />
+          <input type="submit" value="Send" className="button c-button" />
+          <div
+            className="blur c-blur1"
+            style={{ background: 'var(--purple)' }}
+          ></div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
