e1d5ad25b069131c0a1c436aa0dee303571fb416
Ahim11 <ahim.mudbari@gmail.com>
2022-09-20 22:01:36 +0545

    Toogle button refactoring

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Toogle from '../Toogle/Toogle';
 import './Navbar.css';
 
 export const Navbar = () => {
@@ -7,7 +8,7 @@ export const Navbar = () => {
       <div className="n-wrapper">
         <div className="n-left">
           <div className="n-name">Ahimsha</div>
-          <span>toggle btn</span>
+          <Toogle />
         </div>
         <div className="n-right">
           <div className="n-list">
