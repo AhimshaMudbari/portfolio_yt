fa0cb26e87608053445deac412b060a9b2819ca7
Ahim11 <ahim.mudbari@gmail.com>
2022-09-24 21:59:18 +0545

    Card component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,11 +3,19 @@ import './Card.css';
 import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
 import Close from '../../img/close.png';
+import { motion } from 'framer-motion';
 const Card = ({ emoji, heading, detail }) => {
+  const transition = { duration: 2, type: 'spring' };
   return (
     <div className="card">
-      <img src={emoji} alt="" />
-      <span>{heading}</span>
+      <motion.span
+        whileInView={{ left: '5rem' }}
+        initial={{ left: '25%' }}
+        transition={transition}
+      >
+        {heading}
+      </motion.span>
+      <motion.img src={emoji} alt="" />
       <span>{detail}</span>
       <Popup
         trigger={<button className="button c-button">Read More..</button>}
@@ -20,7 +28,7 @@ const Card = ({ emoji, heading, detail }) => {
             <div></div>
             <div className="header">{heading}</div>
             <div className="picture">
-              <img src={emoji} alt="" />
+              <img height={320} width={500} src={emoji} alt="" />
             </div>
             <div className="Content">{detail}</div>
 
