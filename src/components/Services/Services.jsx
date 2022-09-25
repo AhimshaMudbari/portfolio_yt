57893d16cf9160d358945adf03d908c8f9eecc28
Ahim11 <ahim.mudbari@gmail.com>
2022-09-25 21:48:34 +0545

    Services component with framer motion animation and modal

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import './Services.css';
-import HeartEmoji from '../../img/heartemoji.png';
+import Design from '../../img/design.png';
 import Card from '../Card/Card';
-import Glasses from '../../img/glasses.png';
-import Humble from '../../img/humble.png';
+import Programming from '../../img/Programming.png';
+import Database from '../../img/database.png';
 import Resume from './ahimshaResume.pdf';
 
 const Services = () => {
   return (
-    <div className="services">
+    <div className="services Services">
       <div className="awesome">
-        <span>My Awesome</span>
-        <span>Services</span>
+        <span>Things That</span>
+        <span>I love to do...</span>
         <span>
           {' '}
           <li>To code for hours and hours and explore relevant contents.</li>
@@ -25,23 +25,26 @@ const Services = () => {
         <div className="blur s-blur" style={{ background: '#ABF1FF' }}></div>
       </div>
       <div className="cards">
-        <div style={{ left: '17rem' }}>
+        <div style={{ flex: '1' }}>
           <Card
-            emoji={HeartEmoji}
+            emoji={Design}
             heading={`Design`}
             detail={'Canva, Picmonkey, Figma, Photoshop'}
           />
         </div>
-        <div style={{ top: '12rem', left: '-2rem' }}>
+        <hr />
+        <div style={{ flex: '1' }}>
           <Card
-            emoji={Glasses}
+            emoji={Programming}
             heading={`Developing`}
             detail={'Html, Css, JavaScript, React, Php, Java, NodeJS, c#'}
           />
         </div>
+        <hr />
+
         <div style={{ top: '17rem', left: '14rem' }}>
           <Card
-            emoji={Humble}
+            emoji={Database}
             heading={`Databases`}
             detail={'MongoDB, MySQL, GraphQL, Oracle'}
           />
