06da87d1b45369e94421610bde2a12dc135fe067
Ahim11 <ahim.mudbari@gmail.com>
2022-09-25 21:44:06 +0545

    Navbar Component and refactor

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Toogle from '../Toogle/Toogle';
 import './Navbar.css';
+import { Link } from 'react-scroll';
 
 export const Navbar = () => {
   return (
-    <div>
+    <div className="Navbar">
       <div className="n-wrapper">
         <div className="n-left">
           <div className="n-name">Ahimsha</div>
@@ -13,14 +14,31 @@ export const Navbar = () => {
         <div className="n-right">
           <div className="n-list">
             <ul style={{ listStyleType: 'none' }}>
-              <li>Home</li>
-              <li>Services</li>
-              <li>Experience</li>
-              <li>Portfolio</li>
-              <li>Testimonial</li>
+              <Link
+                to="Navbar"
+                smooth={true}
+                spy={true}
+                activeClass="activeClass"
+              >
+                <li>Home</li>
+              </Link>
+              <Link to="Services" smooth={true} spy={true}>
+                <li>Services</li>
+              </Link>
+              <Link to="Experience" smooth={true} spy={true}>
+                <li>Experience</li>
+              </Link>
+              <Link to="Portfolio" smooth={true} spy={true}>
+                <li>Portfolio</li>
+              </Link>
+              <Link to="Testimonial" smooth={true} spy={true}>
+                <li>Testimonial</li>
+              </Link>
             </ul>
           </div>
-          <button className="button n-button">Contact</button>
+          <Link to="Contact" spy={true} smooth={true}>
+            <button className="button n-button">Contact</button>
+          </Link>
         </div>
       </div>
     </div>
