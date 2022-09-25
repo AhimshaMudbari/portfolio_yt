712de45fe9510199fe1b3d6094ed89d5f4e125d4
Ahim11 <ahim.mudbari@gmail.com>
2022-09-25 21:42:26 +0545

    Intro component or landing component

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,44 +1,61 @@
 import React from 'react';
 import './Intro.css';
-import Github from '../../img/github.png';
-import LinkedIn from '../../img/linkedin.png';
-import Instagram from '../../img/instagram.png';
+import LinkedIn from '@iconscout/react-unicons/icons/uil-linkedin';
+import Instagram from '@iconscout/react-unicons/icons/uil-instagram';
+import Github from '@iconscout/react-unicons/icons/uil-github';
 import Splash from '../../img/fire.png';
 import Me from '../../img/ahim.png';
-
+import { motion } from 'framer-motion';
+import { useContext } from 'react';
+import { themeContext } from '../../Context';
 const Intro = () => {
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+  const transition = { duration: 2, type: 'spring' };
   return (
     <div className="intro">
       <div className="i-left">
         <div className="i-name">
-          <span>Hi! It's me</span>
+          <span>Web Developer</span>
           <span>Ahimsha Mudbari</span>
           <span>
             I'm web developer involved in web development, designing and
             learning new things in crazy speed.
           </span>
         </div>
-        <button className="button i-button">Let's connect</button>
+        <p style={{ fontWeight: 'bolder', fontSize: '40px' }}>Find me on...</p>
         <div className="i-icons">
           <a href="https://www.github.com/AhimshaMudbari" target="blank">
             {' '}
-            <img src={Github} alt="" />
+            <Github size={111} color={darkMode ? 'white' : 'black'} />
           </a>
           <a
             href="https://www.linkedin.com/in/ahim-mudbari-4aa326237/"
             target="blank"
           >
-            <img src={LinkedIn} alt="" />
+            <LinkedIn size={111} color="#6195fc" />
           </a>
           <a href="https://www.instagram.com/ahim_2000/" target="blank">
             {' '}
-            <img src={Instagram} alt="" />
+            <Instagram size={111} color="red" />
           </a>
         </div>
       </div>
       <div className="i-right">
-        <img src={Splash} alt="" />
-        <img src={Me} alt="" />
+        <motion.img
+          initial={{ left: '-30%' }}
+          whileInView={{ left: '-1%' }}
+          transition={transition}
+          src={Splash}
+          alt=""
+        />
+        <motion.img
+          initial={{ left: '-40%' }}
+          whileInView={{ left: '-1%' }}
+          transition={transition}
+          src={Me}
+          alt=""
+        />
         <div
           className="blur"
           style={{
